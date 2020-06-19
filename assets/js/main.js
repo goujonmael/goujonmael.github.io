9351b53369b40e4c798fc5a5b33a701429d80b64
Goujon Maël <goujonmael@gmail.com>
2020-06-19 21:14:01 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,12 @@ var settings = {
 
 };
 
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
 (function($) {
 
 	skel.breakpoints({
